d11097313751d877036e9a224e80772b50940276
NNlk05 <helloworldfromnowheresville@gmail.com>
2025-11-03 19:39:26 -0400

    Add fill_area and swap_area methods to Game class for enhanced area manipulation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import time
 END = tk.END
 
 class Game:
@@ -36,20 +35,44 @@ class Game:
     def clear(self):
         self.content = [[self.filler for _ in range(self.width)] for _ in range(self.height)]
         self._refresh_area()
+    
+    def fill_area(self, top_left=(0, 0), bottom_right=None, char=" "):
+        if bottom_right is None:
+            bottom_right = (self.width - 1, self.height - 1)
+        x1, y1 = top_left
+        x2, y2 = bottom_right
+        for y in range(y1, min(y2 + 1, self.height)):
+            for x in range(x1, min(x2 + 1, self.width)):
+                self.content[y][x] = char
+        self._refresh_area()
+    
+    def swap_area(self, top_left1=(0, 0), bottom_right1=None, top_left2=(0, 0)):
+        if bottom_right1 is None:
+            bottom_right1 = (self.width - 1, self.height - 1)
+        x1a, y1a = top_left1
+        x1b, y1b = bottom_right1
+        x2a, y2a = top_left2
+        
+        width = x1b - x1a + 1
+        height = y1b - y1a + 1
+        
+        for y in range(height):
+            for x in range(width):
+                if (0 <= x1a + x < self.width and 0 <= y1a + y < self.height and
+                    0 <= x2a + x < self.width and 0 <= y2a + y < self.height):
+                    self.content[y1a + y][x1a + x], self.content[y2a + y][x2a + x] =  self.content[y2a + y][x2a + x], self.content[y1a + y][x1a + x]
+        self._refresh_area()
 
 if __name__ == "__main__":
     game = Game()
-    # animate filling using after to avoid blocking the mainloop
     def fill_next(x=0, y=0):
         if x >= game.width:
             return
         game.set_char((x, y), "*")
-        # advance coordinates
         nx, ny = x, y + 1
         if ny >= game.height:
             nx += 1
             ny = 0
-        # schedule next step at ~60 FPS
         game.tk.after(int(1000 / 60), lambda: fill_next(nx, ny))
 
     game.tk.after(0, fill_next)
